3a3a0c27c2f5c09662a8e779bd0c79f9a93ce8e9
alexgwalley <awalley1@cs.binghamton.edu>
2020-11-01 22:17:12 -0500

    completing lab9

--- submit/lab9/exercises/cpuid2/cpuid.s
+++ submit/lab9/exercises/cpuid2/cpuid.s
@@ -3,17 +3,21 @@
 	.text
 	.globl get_cpuid
 get_cpuid:
-	pushq   %rcx
-	pushq   %rdx
+	pushq	%r8		#push address of edx return
+	pushq   %rcx		#push address of ecx return
+	pushq   %rdx		#push address of ebx return
 	xorl 	%eax, %eax	#setup cpuid opcode to 0
+	movq 	%rdi, %rax
 	cpuid
 	#largest param in %eax
 	#12-char manufacturer string in ebx, edx, ecx.
-	movl	%eax, (%rdi)	#store eax cpuid result
-	movl	%ebx, (%rsi)	#store ebx cpuid result
+	movl	%eax, (%rsi)	#store eax cpuid result
+	popq	%rax
+	movl	%ebx, (%rax)	#store ebx cpuid result
 	popq	%rax		#pop address for edxP
-	movl    %edx, (%rax)    #store edx cpuid result
+	movl    %edx, (%rax)    #store ecx cpuid result
 	popq    %rax		#pop address for ecxP
-	movl	%ecx, (%rax)	#store ecx cpuid result
+	movl	%ecx, (%rax)	#store edx cpuid result
+	
 	ret
 	
--- submit/lab9/exercises/parity/parity.s
+++ submit/lab9/exercises/parity/parity.s
@@ -4,5 +4,12 @@
 get_parity:
 
 	#@TODO: add code here to set eax to 1 iff edi has even parity
+	testl %edi, %edi
+	jpe .parity_even
+	movl $0, %eax
+	jmp .end
+.parity_even:
+	movl $1, %eax
+.end:
 	ret
 	
