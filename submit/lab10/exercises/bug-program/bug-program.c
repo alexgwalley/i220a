d629ca0be90b767c627b490abc914f914bb70e39
alexgwalley <awalley1@cs.binghamton.edu>
2020-11-08 17:53:29 -0500

    completing lab10

--- submit/lab10/exercises/bug-program/bug-program.c
+++ submit/lab10/exercises/bug-program/bug-program.c
@@ -18,10 +18,10 @@ static struct KeyValue *
 add_key_value(struct KeyValue *keyValues, const char *k, int v)
 {
   //allocate storage for new KeyValue struct
-  struct KeyValue *kv = malloc(sizeof(struct KeyValue *));
+  struct KeyValue *kv = malloc(sizeof(struct KeyValue));
 
   //allocate storage for string pointed to by k
-  char *s = malloc(strlen(k));
+  char *s = malloc(strlen(k) + 1);
 
   if (kv == NULL || s == NULL) { //check if allocations succeeded
     fprintf(stderr, "malloc failure: %s\n", strerror(errno));
@@ -41,7 +41,9 @@ free_key_values(struct KeyValue *keyValues)
 {
   //go thru chain of keyValues
   for (struct KeyValue *p = keyValues; p != NULL; p = p->succ) {
+    free(p->key);
     free(p); //free KeyValue struct
+
   }
 }
 
